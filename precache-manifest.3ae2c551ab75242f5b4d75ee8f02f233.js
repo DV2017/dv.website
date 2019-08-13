c264b7c576d9e945e2d6a328a79bc5ac5a5525ab
DV <deepavarma.in@gmail.com>
2019-08-13 13:45:22 +0200

    Updates

---
+++ precache-manifest.3ae2c551ab75242f5b4d75ee8f02f233.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9149965256b009ff371ea693181d00cd",
+    "url": "/DV2017/dv.website.git/index.html"
+  },
+  {
+    "revision": "b5978de9f29db2e8d009",
+    "url": "/DV2017/dv.website.git/static/css/main.71aeb265.chunk.css"
+  },
+  {
+    "revision": "7db911faebd2f3500565",
+    "url": "/DV2017/dv.website.git/static/js/2.479fc036.chunk.js"
+  },
+  {
+    "revision": "b5978de9f29db2e8d009",
+    "url": "/DV2017/dv.website.git/static/js/main.898c87b2.chunk.js"
+  },
+  {
+    "revision": "4eb853446c9e992bc418",
+    "url": "/DV2017/dv.website.git/static/js/runtime~main.5d6a1bcb.js"
+  },
+  {
+    "revision": "7a8cb07bc0cbdafd07fa4a2018432eba",
+    "url": "/DV2017/dv.website.git/static/media/GreenHeron.7a8cb07b.jpg"
+  },
+  {
+    "revision": "6a89684338b7fa2ced85be8e5b373518",
+    "url": "/DV2017/dv.website.git/static/media/KF.6a896843.jpg"
+  },
+  {
+    "revision": "042e90ab4c642d50f03bfb68b50775fa",
+    "url": "/DV2017/dv.website.git/static/media/bulls.042e90ab.jpg"
+  },
+  {
+    "revision": "eed4de6a239d88b76f738f2c732cdbf0",
+    "url": "/DV2017/dv.website.git/static/media/flowers.eed4de6a.jpg"
+  },
+  {
+    "revision": "2d52772a41f296707dbeb4765cee9429",
+    "url": "/DV2017/dv.website.git/static/media/kestrel.2d52772a.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DV2017/dv.website.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "74c362907475d0d7f0d59d0fb662135c",
+    "url": "/DV2017/dv.website.git/static/media/sass.74c36290.png"
+  },
+  {
+    "revision": "cec39a4f103e99d9cc09554217dd2c74",
+    "url": "/DV2017/dv.website.git/static/media/webpack.cec39a4f.jpg"
+  },
+  {
+    "revision": "b59d5bee377e0c5054aa67fb56ceed2e",
+    "url": "/DV2017/dv.website.git/static/media/wineglass.b59d5bee.jpg"
+  }
+]);
